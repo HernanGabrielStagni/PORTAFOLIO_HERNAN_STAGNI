c9ebf48b199b0a39a292bb0f237329f1fca53022
HernanGabrielStagni <psicologohernanstagni@gmail.com>
2023-09-08 02:28:27 -0300

    LE AGREGUE LA SECCION DE PROYECTOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 //portafolio-hernan-stagni.netlify.app
 //https://github.com/HernanGabrielStagni/PORTAFOLIO_HERNAN_STAGNI.git
 
-import './App.css'
-import NavBar from './COMPONENTS/NavBar'
-import Inicio from './COMPONENTS/Inicio'
-import SobreMi from './COMPONENTS/SobreMi'
+import "./App.css";
+import NavBar from "./COMPONENTS/NavBar";
+import Inicio from "./COMPONENTS/Inicio";
+import SobreMi from "./COMPONENTS/SobreMi";
+import Contact from "./COMPONENTS/Contact";
+import Study from "./COMPONENTS/Study";
+import Proyects from "./COMPONENTS/Proyects";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <main>
-      
-    
-      <NavBar/>
-      <Inicio/>
-      <SobreMi/>
-
-      
+      <NavBar />
+      <Inicio />
+      <SobreMi />
+      <Proyects/>
+      {/* <Study/> */}
+      <Contact />
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/COMPONENTS/Proyects.jsx
@@ -0,0 +1,72 @@
+
+
+
+// Proyects.js
+import React from 'react';
+import './STYLES/CSS_Proyects.css'
+
+function Proyect({ nombre, imagen, githubUrl, netlifyUrl }) {
+  return (
+    <div className="proyect">
+      <img src={imagen} alt={nombre} />
+      <div className="descripcion">
+        <h3>{nombre}</h3>
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">Link to ... GitHub</a>
+        <a href={netlifyUrl} target="_blank" rel="noopener noreferrer">Link to ...Netlify</a>
+      </div>
+    </div>
+  );
+}
+
+function Proyects() {
+  const proyects = [
+    {
+      nombre: 'Pokedex 111111',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    },
+    {
+      nombre: 'Pokedex 22222',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    },
+    {
+      nombre: 'Pokedex 22222',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    }, {
+      nombre: 'Pokedex 22222',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    }, {
+      nombre: 'Pokedex 22222',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    }, {
+      nombre: 'Pokedex 22222',
+      imagen: '/img/0_Pokedex.png',
+      githubUrl: 'https://github.com/HernanGabrielStagni/PORTFOLIO_HERNAN.git',
+      netlifyUrl: 'https://entregable-5-pokedex-hernan-stagni.netlify.app/ ',
+    },
+    
+    // Agrega más proyects aquí
+  ];
+
+  return (
+    <section id='proyects'>
+      <h2>My Proyects</h2>
+      <div className="proyects-container">
+        {proyects.map((proyect, index) => (
+          <Proyect key={index} {...proyect} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Proyects;
